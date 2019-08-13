dcfc0b9b2e84090e969e130fe4d48199b228fb82
Mika Majakorpi <mika.majakorpi@nitor.com>
2019-08-13 14:32:29 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,3 @@
+AAD_CLIENT_ID = '44e9910d-4f2e-4a83-9848-d76f24051d93'
+AAD_TENANT_ID = '687fa259-bdca-476c-881e-e3ab9207b086'
+AAD_IDP_URL = 'https://login.microsoftonline.com/687fa259-bdca-476c-881e-e3ab9207b086/saml2'
