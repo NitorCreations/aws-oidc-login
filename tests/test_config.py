42814c8f3167270f6c6ca0345b76d4b3bdc5efeb
Mika Majakorpi <mika.majakorpi@nitor.com>
2019-09-19 18:03:55 +0300

    fix test asserts for python 3.5

--- tests/test_config.py
+++ tests/test_config.py
@@ -7,7 +7,7 @@ try:
     import configparser
 except ImportError:
     # Fall back to Python 2
-    import ConfigParser as configparser
+    import ConfigParser as configparser  # noqa: F401
 
 args = {"profile": "test-profile",
         "web_console_login": False}
@@ -24,14 +24,14 @@ def test_init_no_profile_found(args_patched):
         config.init()
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 1
-    args_patched.assert_called_once()
+    assert args_patched.call_count == 1
 
 
 def test_init(args_patched, aws_config_patched):
     config.init()
     for mock in aws_config_patched:
-        mock.assert_called_once()
-    args_patched.assert_called_once()
+        mock.call_count == 1
+    args_patched.call_count == 1
 
 
 def test_missing_authority_url(args_patched, aws_config_patched_without_authority_url):
@@ -41,8 +41,8 @@ def test_missing_authority_url(args_patched, aws_config_patched_without_authorit
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 1
     for mock in aws_config_patched_without_authority_url:
-        mock.assert_called_once()
-    args_patched.assert_called_once()
+        mock.call_count == 1
+    args_patched.call_count == 1
 
 
 @pytest.fixture
